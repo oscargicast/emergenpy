cbbf8a85e89ad18915b7f748e8509b578b83c7af
Oscar Giraldo Castillo <oscar.gi.cast@gmail.com>
2014-06-30 05:25:02 -0500

    parse data in a vanilla way

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import urllib2
+import socket
 from urllib2 import URLError, HTTPError
+from lxml import html
+from lxml import etree
 
 
 url = 'http://www.bomberosperu.gob.pe/po_diario.asp'
@@ -13,12 +16,35 @@ while response is None:
         response = urllib2.urlopen(request, timeout=2)
         print 'url is opened!'
     except HTTPError as e:
-            print 'The server couldn\'t fulfill the request.'
-            print 'Error code: ', e.code
-            print '-'*30
+        print 'The server couldn\'t fulfill the request.'
+        print 'Error code: ', e.code
+        print '-' * 30
     except URLError, e:
         print e.reason
-        print '-'*30
+        print '-' * 30
+    except socket.timeout, e:
+        print "There was an error: %r" % e
+
+
+tree = html.fromstring(response.read())
+events = tree.xpath('//tr[@class="TextoContenido"]')
+print len(events)
+
+emergency = []
+for event in events:
+    item = etree.tostring(event, pretty_print=True)
+    elem = event.xpath('.//div/text()')
+    state = event.xpath('.//div/font/text()')
+    if elem:
+        elem.append(state[0])
+        for index, item in enumerate(elem):
+            elem[index] = ' '.join(
+                elem[index].replace(u'\xa0', '').strip().split()
+            )
+        emergency.append(elem)
+
+print emergency
+print len(emergency)
 
 print response.info().get('date')
 print response.info().get('content-length')
